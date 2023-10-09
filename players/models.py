194c035791be956ab93cdde19ec2f5c657363de6
Abhishek Kumar <deepython05@gmail.com>
2023-10-09 17:14:32 +0530

    setup

--- players/models.py
+++ players/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from passlib.hash import pbkdf2_sha256
-
+from django.contrib.auth.models import User
 
 # Create your models here.
-class RegisterUser(models.Model):
+class RegisterUser(User):
     account_type_choices = [
         ('A', 'Administrator'),
         ('P', 'Player')
     ]
     user_id = models.CharField(max_length=255, blank=True)
-    password = models.CharField(max_length=255, blank=True)
     identity = models.CharField(max_length=255, blank=True)
-    email = models.CharField(max_length=255, blank=True)
     mobile_number = models.CharField(max_length=20, blank=True)
     whatsapp_number = models.CharField(max_length=20, blank=True)
     account_type = models.CharField(max_length=10, choices=account_type_choices, default='A')
--- players/views.py
+++ players/views.py
@@ -3,20 +3,6 @@ from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
-
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserRegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('home')  # Replace 'home' with your desired redirect URL
-    else:
-        form = CustomUserRegistrationForm()
-    
-    return render(request, 'your_app_name/register.html', {'form': form})
-
-
 class RegisterLoginView(LoginView):
     template_name = 'players/login.html'
 
